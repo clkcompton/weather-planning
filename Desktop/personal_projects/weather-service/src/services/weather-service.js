9f19cbb860d47a2fb278da0aac0a17a6f5b2cb32
cori <clkcompton@gmail.com>
2021-04-24 15:34:13 -0500

    connected weather api and crud operations for user; Created endpoints

---
+++ Desktop/personal_projects/weather-service/src/services/weather-service.js
@@ -0,0 +1,20 @@
+const fetch = require('node-fetch');
+
+async function getForecast() {
+
+  const response = await fetch('https://api.openweathermap.org/data/2.5/onecall?lat=33.44&lon=-94.04&exclude=hourly,minutely,current,alerts&units=imperial&appid=083fb268e87096ea29c7db4175b59ef6');
+  const body = await response.json();
+
+  const result = body.daily.map(day => {
+    return {
+      dayOfWeek: new Date((day.dt)*1000).toLocaleDateString('en-US', {weekday: 'long'}),
+      maxTemp: day.temp.max,
+      minTemp: day.temp.min,
+      weatherDescription: day.weather[0].main
+    }});
+  console.log(result);
+
+  return result;
+}
+
+module.exports = getForecast;
